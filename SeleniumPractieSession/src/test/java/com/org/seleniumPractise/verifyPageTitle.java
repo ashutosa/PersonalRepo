8b943dea8bcb1ef523b7b545b20102b36a5f7997
ashutosa <pattajoshi1@gmail.com>
2018-11-04 17:09:24 +0530

    Updated VerifyTitle java

--- SeleniumPractieSession/src/test/java/com/org/seleniumPractise/verifyPageTitle.java
+++ SeleniumPractieSession/src/test/java/com/org/seleniumPractise/verifyPageTitle.java
@@ -11,12 +11,13 @@ public class verifyPageTitle {
 	public void verifyTite() {
 		System.setProperty("webdriver.gecko.driver", "./src/main/resources/geckodriver.exe");
 		WebDriver driver = new FirefoxDriver();
+		System.out.println("Navigating to Facebook");
 		driver.get("https://www.facebook.com/");
 		driver.manage().window().maximize();
 		String title = driver.getTitle();
 		System.out.println("Title of the page  :: " + title);
-		Assert.assertEquals(title, "Facebook – log in or sign up");
-		Assert.assertTrue(title.contains("Facebook – log in or sign up"), "Title matched");
+		Assert.assertEquals(title, "Facebook â€“ log in or sign up");
+		Assert.assertTrue(title.contains("Facebook â€“ log in or sign up"), "Title matched");
 	    driver.close();
 	}
 }
